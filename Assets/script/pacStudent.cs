78ae51b9a84a70f13cff906c2ad6d0a0a8e662d1
thnphu1 <Thien.P.Tran-1@student.uts.edu.au>
2022-09-25 20:55:48 +1000

    85% PacStudent MoveMent

--- Assets/script/pacStudent.cs
+++ Assets/script/pacStudent.cs
@@ -6,6 +6,8 @@ public class pacStudent : MonoBehaviour
 {
 
     public Movement movement { get; private set; }
+    public AudioSource walk;
+    public Animator pacStudent_0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,22 +20,30 @@ public class pacStudent : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             this.movement.SetDirection(Vector2.up);
+            pacStudent_0.SetTrigger("Up");
+            walk.Play();
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
             this.movement.SetDirection(Vector2.down);
+            pacStudent_0.SetTrigger("Down");
+            walk.Play();
         }
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             this.movement.SetDirection(Vector2.left);
+            pacStudent_0.SetTrigger("Left");
+            walk.Play();
         }
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             this.movement.SetDirection(Vector2.right);
+            pacStudent_0.SetTrigger("Right");
+            walk.Play();
         }
 
-        float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
-        this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
-         
+        //float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
+        //this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+
     }
 }
